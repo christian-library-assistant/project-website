aa5e4261ea57d733c0d6f24e73808aed8203229b
Zhonglin-Niu <zn23@calvin.edu>
2025-05-07 16:10:38 -0400

    update the config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://christian-library-assistant.github.io/",
+  base: "project-website",
   integrations: [tailwind()]
 });
